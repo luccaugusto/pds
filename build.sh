b5fc396fd7ea87a37cae985fe7bd8832aca08139
lrr68 <luccaaugusto@protonmail.ch>
2020-09-09 19:11:35 -0300

    target _blank correct now

--- build.sh
+++ build.sh
@@ -5,10 +5,15 @@ JEKYLL_ENV=production &&
 bundle exec jekyll build &&
 
 cd _site
-rm episodios/*
+rm episodios/*.mp3
+rm monologos/*.mp3
 rm *.sh
 cd ..
 
 #sobe so o ultimo episodio, os outros ja estao no servidor
 ULTIMO=$(ls episodios | tail -1)
 cp episodios/$ULTIMO _site/episodios/
+
+#sobe so o ultimo monologo, os outros ja estao no servidor
+ULTIMO=$(ls monologos | tail -1)
+cp monologos/$ULTIMO _site/monologos/
