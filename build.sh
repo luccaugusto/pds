4f5e5c9ef8c11d1d1177d8d04ce05cfd6e1dab24
lrr68 <luccaaugusto@protonmail.ch>
2020-09-24 13:59:47 -0300

    atualizando site

--- build.sh
+++ build.sh
@@ -4,16 +4,16 @@ bundle update &&
 JEKYLL_ENV=production && 
 bundle exec jekyll build &&
 
-cd _site
-rm episodios/*.mp3
-rm monologos/*.mp3
-rm *.sh
-cd ..
+cd _site &&
+rm episodios/*.mp3 &&
+rm monologos/*.mp3 &&
+rm *.sh &&
+cd .. &&
 
 #sobe so o ultimo episodio, os outros ja estao no servidor
-ULTIMO=$(ls episodios | tail -1)
-cp episodios/$ULTIMO _site/episodios/
+ULTIMO=$(ls episodios | tail -1) &&
+cp episodios/$ULTIMO _site/episodios/ &&
 
 #sobe so o ultimo monologo, os outros ja estao no servidor
-ULTIMO=$(ls monologos | tail -1)
+ULTIMO=$(ls monologos | tail -1) &&
 cp monologos/$ULTIMO _site/monologos/
