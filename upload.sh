b54de930342be2ae7f8ba2d07829afb585840776
lrr68 <luccaaugusto@protonmail.ch>
2020-10-15 10:19:14 -0300

    melhorando script de upload para atualizar o git

--- upload.sh
+++ upload.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
+echo "UPLOADING"
 rsync -avz _site/ root@luccaaugusto.xyz:/var/www/pds.luccaaugusto.xyz/html/
+
+echo "UPDATE GIT"
+EP=$(ls _site/episodios/)
+MON=$(ls _site/monologos/)
+
+[ "$EP" ] && MSG="update relacionado a "$EP
+[ "$MON" ] && MSG="update relacionado a "$MON
+git add .
+git commit -m "$MSG"
+git push
