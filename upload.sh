3a413a22867d912b1c7af20a711c161cf8fe437e
luccaugusto <lucca@luccaaugusto.xyz>
2024-04-03 10:53:55 -0300

    add not by ai badges

--- upload.sh
+++ upload.sh
@@ -3,12 +3,12 @@
 echo "UPLOADING"
 rsync -avz _site/ root@luccaaugusto.xyz:/var/www/pds.luccaaugusto.xyz/html/
 
-echo "UPDATE GIT"
-EP=$(ls _site/episodios/)
-MON=$(ls _site/monologos/)
-
-[ "$EP" ] && MSG="update relacionado a "$EP
-[ "$MON" ] && MSG="update relacionado a "$MON
-git add .
-git commit #-m "$MSG"
-git push
+#echo "UPDATE GIT"
+#EP=$(ls _site/episodios/)
+#MON=$(ls _site/monologos/)
+#
+#[ "$EP" ] && MSG="update relacionado a "$EP
+#[ "$MON" ] && MSG="update relacionado a "$MON
+#git add .
+#git commit #-m "$MSG"
+#git push
